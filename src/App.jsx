ccec3a789a8b7389ca0134ed11c8dbe9f42b21d2
vx5uu <vx5uu@5NXKE.shutdown>
2026-03-08 09:04:30 +0530

    First-Commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form className='flex justify-between'>
+        <input 
+        type="text" 
+        placeholder='Heading' 
+        className=''
+        />
+        <input 
+        type="text" 
+        placeholder='Notes'
+        className=''
+        />
+      </form>
+    </div>
+  )
+}
+
+export default App
